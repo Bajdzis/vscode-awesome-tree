f7a042a2f39be685909fd9df281b8bee76e2a109
Bajdzis <rafal612b@wp.pl>
2019-09-26 19:21:37 +0200

    chore: fix test for Windows OS paths

---
+++ src/__mocks__/path.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { changeToUnixSlashes } from '../strings/changeToUnixSlashes';
+
+const pathNode: typeof path = jest.requireActual('path');
+
+export const join = jest.fn((...args) => changeToUnixSlashes(pathNode.join(...args)));
+
+export const dirname = jest.fn((args) => changeToUnixSlashes(pathNode.dirname(args)));
+
+export const basename = jest.fn((args) => changeToUnixSlashes(pathNode.basename(args)));
