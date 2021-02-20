981e5b34ff4b8d56e7540aaf3667c6afd86816ad
Bajdzis <rafal612b@wp.pl>
2021-02-20 22:35:46 +0100

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,12 @@ import { findWorkspacePath } from './commands/saveAsTemplate';
 import { getAllFilesPath } from './fileSystem/getAllFilesPath';
 import { createStore } from './store';
 import { createNewTemplate, onDidChange, onDidCreate, onDidDelete, onRegisterWorkspace, renameDirectory, WatchFileSystemParam } from './store/action/files/files';
+interface MinInfoAboutSymbol {
+    value: string;
+    rangeStart: vscode.Position;
+    rangeEnd: vscode.Position;
+    kind: vscode.SymbolKind;
+}
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -67,6 +73,69 @@ export function activate(context: vscode.ExtensionContext) {
             uri,
             workspacePath
         }));
+    }));
+
+
+    context.subscriptions.push(vscode.commands.registerCommand('extension.renameFile', async (uri: vscode.Uri) => {
+
+        function flatChildrenObject (arr : (vscode.SymbolInformation | vscode.DocumentSymbol)[]): MinInfoAboutSymbol[] {
+
+            const result =  arr.reduce((old,item): MinInfoAboutSymbol[] => {
+                console.log({item});
+                if(item instanceof vscode.DocumentSymbol) {
+                    let additional: MinInfoAboutSymbol[] = flatChildrenObject(item.children);
+                    
+                    return [
+                        ...old,
+                        {
+                            value: item.name,
+                            rangeStart: item.range.start,
+                            rangeEnd: item.range.end,
+                            kind: item.kind
+                        },
+                
+                        ...additional];
+                } else  if(item instanceof vscode.SymbolInformation) {
+
+                    // @ts-ignore
+                    let additional: MinInfoAboutSymbol[] = flatChildrenObject(item.children);
+                    return [
+                        ...old,
+                        {
+                            value: item.name,
+                            rangeStart: item.location.range.start,
+                            rangeEnd: item.location.range.end,
+                            kind: item.kind
+                        },
+                    
+                        ...additional];
+                }
+
+                return old;
+            }, [] as MinInfoAboutSymbol[]);
+
+
+            return result; 
+
+        }
+
+        let success = await vscode.commands.executeCommand<Promise<(vscode.SymbolInformation | vscode.DocumentSymbol)[]>>('vscode.executeDocumentSymbolProvider', uri);
+
+        console.log(success);
+
+        success && console.log(flatChildrenObject(success) );
+        
+        // const workspacePath = findWorkspacePath(uri.fsPath);
+
+        // if (workspacePath === undefined) {
+        //     vscode.window.showWarningMessage(`Can't find workspace directory for file: '${uri.fsPath}'`);
+        //     return;
+        // }
+
+    // store.dispatch(createNewTemplate.started({
+    //     uri,
+    //     workspacePath
+    // }));
     }));
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.renameDirectory', (uri: vscode.Uri) => {
